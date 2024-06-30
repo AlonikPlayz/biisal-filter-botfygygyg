b748300f6c350c6df7a5efc90539114e939f447b
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2024-06-30 21:43:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '22235438'))
 API_HASH = environ.get('API_HASH', 'd061c8a617c91bbb3bca568d32b8e2eb')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6459065261:AAFfLRElYhcQsB3Sd9gH_ZXRtqrAFaq43ic')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1276438328').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/alonik1")
