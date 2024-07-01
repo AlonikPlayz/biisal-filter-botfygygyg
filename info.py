f52ab78dc045148b08bb62d9a782ff7961867256
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2024-07-01 11:50:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-4274277446'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002203739880')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002124862190'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1001805268565'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002203739880'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '8080')
