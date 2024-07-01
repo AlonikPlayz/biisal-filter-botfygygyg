356113bd0c61d62966ad59b30359643ffb60ca55
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2024-07-01 15:32:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,7 +7,7 @@ import pytz
 from datetime import timedelta
 from datetime import datetime as dt
 from Script import script
-from pyrogram import Client, filters, enums
+from pyrogram import Client, app, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , ForceReply
 from database.ia_filterdb import Media, get_file_details, get_bad_files, unpack_new_file_id
